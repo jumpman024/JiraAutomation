dff368bac6413bf0db2f983da445c01ad71c3f61
Kostya <konstantin.bezsmertnyy@gmail.com>
2022-07-22 13:52:43 +0300

    commit #14 for jenkins config test

--- src/main/java/uiUtils/core/BaseSeleniumTest.java
+++ src/main/java/uiUtils/core/BaseSeleniumTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import readProperties.ConfigProvider;
@@ -18,8 +19,18 @@ abstract public class BaseSeleniumTest {
 
     @BeforeTest
     public void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox"); // Bypass OS security model, MUST BE THE VERY FIRST OPTION
+//        options.addArguments("--remote-debugging-port=9222");
+//        options.addArguments("--headless");
+//        options.setExperimentalOption("useAutomationExtension", false);
+//        options.addArguments("start-maximized"); // open Browser in maximized mode
+//        options.addArguments("disable-infobars"); // disabling infobars
+//        options.addArguments("--disable-extensions"); // disabling extensions
+//        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.get(ConfigProvider.URL);
 //        driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
